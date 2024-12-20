0000da623e9df0fd1c125e123f3c3fa47acb340f
AkbarshoxOtaboev <otaboevakbar96@gmail.com>
2024-12-20 15:29:59 +0300

    Initializing project

---
+++ src/main/java/uz/bot/deliverybot/DeliveryBotApplication.java
@@ -0,0 +1,13 @@
+package uz.bot.deliverybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/bot/deliverybot/DeliveryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.bot.deliverybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
